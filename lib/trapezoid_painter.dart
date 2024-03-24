89a55c5171a73a01a11a06f99fc551aa3e13b758
Static_Reasons <827391398@qq.com>
2024-03-25 03:26:32 +0800

    trapezoid_painter.dart: 可选择相对长度或绝对长度

--- lib/trapezoid_painter.dart
+++ lib/trapezoid_painter.dart
@@ -56,14 +56,21 @@ import 'dart:math' as math;
 //   }
 // }
 
+enum LocationType{
+  relative,
+  absolute
+}
+
 class TrapezoidPainter extends CustomPainter {
 
   final Axis axis;
   final double topStart, topEnd, bottomStart, bottomEnd;
   final Color color;
-  
+  final LocationType locationType;
+
   const TrapezoidPainter({
     this.axis = Axis.horizontal,
+    this.locationType = LocationType.relative,
     this.topStart = 0,
     this.topEnd = 1,
     this.bottomStart = 0,
@@ -78,16 +85,30 @@ class TrapezoidPainter extends CustomPainter {
       ..style = PaintingStyle.fill;
 
     final path = Path();
-    if (axis == Axis.horizontal) {
-      path.moveTo(size.width * topStart, 0);
-      path.lineTo(size.width * topEnd, 0);
-      path.lineTo(size.width * bottomEnd, size.height);
-      path.lineTo(size.width * bottomStart, size.height);
-    } else {
-      path.moveTo(0, size.height * bottomStart);
-      path.lineTo(0, size.height * bottomEnd);
-      path.lineTo(size.width, size.height * topEnd);
-      path.lineTo(size.width, size.height * topStart);
+    if(locationType == LocationType.relative) {
+      if (axis == Axis.horizontal) {
+        path.moveTo(size.width * topStart, 0);
+        path.lineTo(size.width * topEnd, 0);
+        path.lineTo(size.width * bottomEnd, size.height);
+        path.lineTo(size.width * bottomStart, size.height);
+      } else {
+        path.moveTo(0, size.height * bottomStart);
+        path.lineTo(0, size.height * bottomEnd);
+        path.lineTo(size.width, size.height * topEnd);
+        path.lineTo(size.width, size.height * topStart);
+      }
+    }else{
+      if (axis == Axis.horizontal) {
+        path.moveTo(topStart, 0);
+        path.lineTo(topEnd, 0);
+        path.lineTo(bottomEnd, size.height);
+        path.lineTo(bottomStart, size.height);
+      } else {
+        path.moveTo(bottomStart, 0);
+        path.lineTo(bottomEnd, 0);
+        path.lineTo(topEnd, size.height);
+        path.lineTo(topStart, size.height);
+      }
     }
     path.close();
 

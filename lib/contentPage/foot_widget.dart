09cf1e87629768e6d1bc3e2e2c01bb7687487c1a
Imitater967 <imitater967@qq.com>
2024-03-31 17:06:32 +0800

    优化

---
+++ lib/contentPage/foot_widget.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../media_type.dart';
+
+class FootWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+
+    var size = MediaQuery.of(context).size;
+    var scaling = min(1.0, size.width / MediaType.medium.width);
+
+    var footerTextStyle = TextStyle(
+        color: Theme.of(context).colorScheme.onSecondary,
+        fontSize: 20 * scaling,
+        letterSpacing: 3 * scaling
+    );
+
+    // 底部
+    return Container(
+      height: 100 * scaling, // 设置容器的高度
+      color: Theme.of(context).colorScheme.secondary,
+      padding: EdgeInsets.all(4.0 * scaling),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            '这里的第一行是各种声明,比如:本报所有内容均由用户生成,本报只做收集与整理,所有内容均与本报无关,就像这样,位占位占位占位占位占',
+            style: footerTextStyle,
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            '下面一行可以放一些鸣谢声明,像:本报版面由creepebucket设计,占位占位占位占位位占位占位占位占位占位占位占位占位占位',
+            style: footerTextStyle,
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            '在这里放一些东西,就像: 备案号:X公网安备XXX 支持我们:www.afdian.net/XXX 开源链接:github.com/XXX',
+            style: footerTextStyle,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+}

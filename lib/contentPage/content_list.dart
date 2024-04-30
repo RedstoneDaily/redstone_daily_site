08fbd51de93dce7298218136807efc2fa42fe556
Static_Reasons <827391398@qq.com>
2024-04-30 17:35:52 +0800

    dart define

--- lib/contentPage/content_list.dart
+++ lib/contentPage/content_list.dart
@@ -23,6 +23,7 @@ double inverseLerp(double a, double b, double x) {
 // 网站内容列表
 class ContentList extends StatefulWidget {
   const ContentList({super.key, required this.year, required this.month, required this.day});
+
   final String year;
   final String month;
   final String day;
@@ -39,13 +40,13 @@ class _ContentListState extends State<ContentList> {
   // for large device, it is free
   static const double itemPadding = 20;
 
-  late Future<NewsPaper> _futureBuildItems;
+  late Future<NewsPaper> _futureFetchNewspaper;
 
   // 异步获取json字符串
   Future<String> fetchJson(BuildContext context) async {
     // return DefaultAssetBundle.of(context).loadString("assets/demo.json");
     // fetch data from url api
-    const String apiHost = String.fromEnvironment('API_BASE_URL', defaultValue: 'localhost');
+    const String apiHost = String.fromEnvironment('API_HOST', defaultValue: 'localhost');
     Uri uri = Uri.http(apiHost, '/daily', {
       'yy': widget.year,
       'mm': widget.month,
@@ -54,18 +55,17 @@ class _ContentListState extends State<ContentList> {
 
     try {
       final response = await http.get(uri);
-
       if (response.statusCode == 200) {
         // 如果请求成功，返回JSON字符串
         return response.body;
       } else {
         // 如果请求失败，抛出一个错误
-        throw Exception('Failed to load JSON. Status code: ${response.statusCode}');
+        throw Exception('Failed to load the newspaper content. Status code: ${response.statusCode}');
       }
     } catch (error) {
       // 处理任何其他类型的错误
       print('Error occurred: $error');
-      rethrow; // 你可以选择在这里重新抛出错误或返回一个错误消息
+      rethrow;
     }
   }
 
@@ -73,7 +73,7 @@ class _ContentListState extends State<ContentList> {
   void initState() {
     super.initState();
     // 获取内容信息json，并构建内容组件列表
-    _futureBuildItems = fetchJson(context).then((str) => newsPaperFromJson(str));
+    _futureFetchNewspaper = fetchJson(context).then((str) => newsPaperFromJson(str));
   }
 
   @override
@@ -82,13 +82,19 @@ class _ContentListState extends State<ContentList> {
       if (snapshot.connectionState == ConnectionState.waiting) {
         // 当Future还未完成时，显示加载中的UI
         return SliverFillRemaining(
-            child: Container(
-          color: RDColors.white.surface,
-          child: const Center(child: CircularProgressIndicator()),
-        ));
+          child: Container(
+            color: RDColors.white.surface,
+            child: const Center(child: CircularProgressIndicator()),
+          ),
+        );
       } else if (snapshot.hasError) {
         // 当Future发生错误时，显示错误提示的UI
-        return Text('Error: ${snapshot.error}');
+        return SliverFillRemaining(
+          child: Container(
+            color: RDColors.white.surface,
+            child: Center(child: Text('无法加载内容，请刷新页面重试', style: TextStyle(color: RDColors.white.onSurface))),
+          ),
+        );
       } else {
         // 当Future成功完成时，显示数据
         return buildWithNewsPaper(snapshot.data!, context);
@@ -96,7 +102,7 @@ class _ContentListState extends State<ContentList> {
     }
 
     return FutureBuilder(
-      future: _futureBuildItems,
+      future: _futureFetchNewspaper,
       builder: builder,
     );
   }
